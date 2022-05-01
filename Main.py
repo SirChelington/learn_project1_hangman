d561f52287844c5c34ba745b3266213c2c5a7fca
SirChelington <SirChelington@mail.de>
2022-05-01 12:43:47 +0200

    code einheitlich und hübsch gemacht, copy-paste code entfernt.

--- Main.py
+++ Main.py
@@ -1,51 +1,53 @@
-### Das Quiz das die Welt verändert! ### 
+### Das Quiz das die Welt verändert! ###
 import re
 
-#Variablen
-dasWort = str("Kerze").upper()
-gw = str("")
-hangman = ["_","_","_","_","_"]
-gelöst = ""
+# Variablen
+the_word = "Kerze".upper()
+guessed_letter = ""
+place_holder = ["_", "_", "_", "_", "_"]
+result = ""
 ###funktionen.
 
-#user input
-def programm_start(dasWort, gw, gelöst):
-    if gelöst != dasWort:
-        gw = str(input()).upper()
-        gelöst = lettercheck(gw)
-        return gelöst
+# user input
+def program_start(the_word, guessed_letter, result):
+    if result != the_word:
+        guessed_letter = str(input()).upper()
+        return letter_check(guessed_letter)
 
-#Check auf länge und Buchstaben             
-def lettercheck(gw):
-    if len(gw) == 1 and re.search(r"[a-zA-Z]+",gw):
-        gelöst = buchstabenSuche(gw, dasWort)
-        return gelöst
+
+# Check auf länge und Buchstaben
+def letter_check(guessed_letter):
+    if len(guessed_letter) == 1 and re.search(r"[a-zA-Z]+", guessed_letter):
+        return letter_search(guessed_letter, the_word)
     else:
         print("Bitte geben Sie nur einen Buchstaben ein.")
 
-#suche nach passenden buchstaben.
-def buchstabenSuche(gw, dasWort):
-    if re.search(gw, dasWort):
-        #gucken an welcher Stelle der Buchstabe steht.
-        bPosition = [i for i, x in enumerate(dasWort) if x == gw]
-        gelöst = user_Preview(gw, bPosition)
-        return gelöst
-    else: 
+
+# suche nach passenden buchstaben.
+def letter_search(guessed_letter, the_word):
+    if re.search(guessed_letter, the_word):
+        # gucken an welcher Stelle der Buchstabe steht.
+        letter_position = []
+        for index in enumerate(the_word):
+            if index[1] == guessed_letter:
+                letter_position.append(index[0])
+        return user_Preview(guessed_letter, letter_position)
+    else:
         print("Leider Falsch")
 
-#Gelöste Buchstaben an die richtige Stelle schreiben.
-def user_Preview(gw, bPosition):
-    for w in range(len(bPosition)):
-        hangman[bPosition[w]] = gw
-        preview = "".join(hangman)
+
+# Gelöste Buchstaben an die richtige Stelle schreiben.
+def user_Preview(guessed_letter, letter_position):
+    for w in range(len(letter_position)):
+        place_holder[letter_position[w]] = guessed_letter
+        preview = "".join(place_holder)
     print(preview.upper())
-    gelöst = preview
-    return gelöst
-        
+    result = preview
+    return result
 
 
-#Ausführung
-while gelöst != dasWort:
-    gelöst = programm_start(dasWort, gw, gelöst)
+# Ausführung
+while result != the_word:
+    result = program_start(the_word, guessed_letter, result)
 else:
     print("Gut gemacht!")
